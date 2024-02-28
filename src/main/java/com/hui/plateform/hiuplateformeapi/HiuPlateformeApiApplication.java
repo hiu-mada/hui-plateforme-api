378a202e25cc293351471a5feb52e8bfed7977ee
FanomezanaNat <hei.fanomezana.2@gmail.com>
2024-02-28 14:06:16 +0300

    feat: generated project

---
+++ src/main/java/com/hui/plateform/hiuplateformeapi/HiuPlateformeApiApplication.java
@@ -0,0 +1,13 @@
+package com.hui.plateform.hiuplateformeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HiuPlateformeApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HiuPlateformeApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hui/plateform/hiuplateformeapi/HiuPlateformeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hui.plateform.hiuplateformeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HiuPlateformeApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
