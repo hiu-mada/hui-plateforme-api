7f2149a3cb7dbe164eb1028d1ea3a256147dbe5a
FanomezanaNat <hei.fanomezana.2@gmail.com>
2024-02-28 14:50:51 +0300

    feat: update CI workflows

--- src/test/java/com/hui/plateform/hiuplateformeapi/HiuPlateformeApiApplicationTests.java
+++ src/test/java/com/hui/plateform/hiuplateformeapi/HiuPlateformeApiApplicationTests.java
@@ -1,11 +1,8 @@
 package com.hui.plateform.hiuplateformeapi;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class HiuPlateformeApiApplicationTests {
 
-  @Test
-  void contextLoads() {}
 }
