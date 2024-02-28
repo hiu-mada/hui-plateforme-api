5ea9460e01dd4fb07c01e3149c6a80e423d04c0b
FanomezanaNat <hei.fanomezana.2@gmail.com>
2024-02-28 14:51:51 +0300

    chore: reformat code

--- src/test/java/com/hui/plateform/hiuplateformeapi/HiuPlateformeApiApplicationTests.java
+++ src/test/java/com/hui/plateform/hiuplateformeapi/HiuPlateformeApiApplicationTests.java
@@ -3,6 +3,4 @@ package com.hui.plateform.hiuplateformeapi;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class HiuPlateformeApiApplicationTests {
-
-}
+class HiuPlateformeApiApplicationTests {}
