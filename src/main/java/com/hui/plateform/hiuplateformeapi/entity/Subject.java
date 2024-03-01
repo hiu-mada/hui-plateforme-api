48a6ec4fdfaf28b0d53926050ee9f63f62486852
nirinaHelena <hei.nirina.7@gmail.com>
2024-03-01 22:00:48 +0300

    refactor: rename entity table name Subject to subject_table

--- src/main/java/com/hui/plateform/hiuplateformeapi/entity/Subject.java
+++ src/main/java/com/hui/plateform/hiuplateformeapi/entity/Subject.java
@@ -14,9 +14,16 @@ import lombok.*;
 @Table(name = "subject_table")
 public class Subject {
     @Id
+    @Column(name = "id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private String id;
+
+    @Column(name = "title")
     private String title;
+
+    @Column(name = "description")
     private String description;
+
+    @Column(name = "max_score")
     private Float max_score;
 }
