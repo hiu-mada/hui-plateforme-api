b80af82c4abfbd322718a0a7c1283f7970f15749
nirinaHelena <hei.nirina.7@gmail.com>
2024-03-02 08:19:46 +0300

    feat: create table vote

---
+++ src/main/java/com/hui/plateform/hiuplateformeapi/entity/Vote.java
@@ -0,0 +1,22 @@
+package com.hui.plateform.hiuplateformeapi.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.*;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Table(name = "vote_table")
+public class Vote {
+
+    @Column(name = "id_challenger")
+    private String idChallenger;
+
+    @Column(name = "id_user")
+    private String idUser;
+}
