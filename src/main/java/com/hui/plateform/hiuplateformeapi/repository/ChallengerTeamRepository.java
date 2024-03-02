250a270c45e6663acd61483dec0f5ad3e7ee567e
nirinaHelena <hei.nirina.7@gmail.com>
2024-03-02 07:50:21 +0300

    feat: create challenger team repository

---
+++ src/main/java/com/hui/plateform/hiuplateformeapi/repository/ChallengerTeamRepository.java
@@ -0,0 +1,7 @@
+package com.hui.plateform.hiuplateformeapi.repository;
+
+import com.hui.plateform.hiuplateformeapi.entity.ChallengerTeam;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ChallengerTeamRepository extends JpaRepository<ChallengerTeam, String> {
+}
