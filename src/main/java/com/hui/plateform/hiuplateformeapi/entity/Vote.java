fde4ed89a4b4de9d2af606fde3373d4cfefb8f80
nirinaHelena <hei.nirina.7@gmail.com>
2024-03-06 21:06:04 +0300

    fix: add column id in entity vote

--- src/main/java/com/hui/plateform/hiuplateformeapi/entity/Vote.java
+++ src/main/java/com/hui/plateform/hiuplateformeapi/entity/Vote.java
@@ -14,6 +14,7 @@ import static jakarta.persistence.GenerationType.IDENTITY;
 public class Vote {
     @Id
     @GeneratedValue(strategy = IDENTITY)
+    @Column(name = "id")
     private String id;
 
     @Column(name = "id_challenger")
