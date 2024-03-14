c2bf5f60019fec1c597b932dea0c6a375345c20e
FanomezanaNat <124127238+FanomezanaNat@users.noreply.github.com>
2024-03-14 22:25:36 +0300

    Update university and user scripts
    * feat add attribut role in user script migration
    
    * feat: insert data for university table
    
    * fix: update user table
    
    * fix: update script inserting university

---
+++ src/main/resources/db/migration/V42_1__Insert_university.sql
@@ -0,0 +1,8 @@
+INSERT INTO "university_table" VALUES ('ESPA','Ecole Supérieur Polytechnique d''Antananarivo');
+INSERT INTO "university_table" VALUES ('ENI,lorem ipsum','Ecole National de l''Informatique');
+INSERT INTO "university_table" VALUES ('EMIT','Ecole de Management et Innovation Technologique ');
+INSERT INTO "university_table" VALUES ('ASJA','Athénée Saint Joseph Antsirabe');
+INSERT INTO "university_table" VALUES ('MISA','Mathématique Informatique et Statistiques Appliquées');
+INSERT INTO "university_table" VALUES ('ESTI','Ecole Superieur des Technologies de l''Information');
+INSERT INTO "university_table" VALUES ('UPH','Université privée Hay');
+INSERT INTO "university_table" VALUES ('IES-SI','Institut d''Enseignement Supérieur de Soavinadriana Itasy ');
