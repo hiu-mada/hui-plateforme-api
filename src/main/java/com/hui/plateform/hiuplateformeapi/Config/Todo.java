60fd32c82032d25b9f1ff37ca8e1f5826e83e53f
nirinaHelena <hei.nirina.7@gmail.com>
2024-03-18 16:02:28 +0300

    docs: add todo list, all the things we must do

---
+++ src/main/java/com/hui/plateform/hiuplateformeapi/Config/Todo.java
@@ -0,0 +1,13 @@
+package com.hui.plateform.hiuplateformeapi.Config;
+
+public class Todo {
+    /**
+     * TODO: update api.yaml
+     * TODO: add return value entity to add, update, create function
+     * TODO: add OAuth2 authentication method
+     * TODO: check authorisiation with all entity
+     * TODO: add test
+     * TODO: update all entity attribut to be like the ressources
+     * TODO: add endpoint get all flag per team
+     */
+}
