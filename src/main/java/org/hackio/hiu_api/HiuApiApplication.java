4bb8c9cfbce73b5c89692a96f8d66e175462cec1
miharyjoel <miharyjoel@gmail.com>
2024-03-20 16:38:14 +0300

    feat: init project

---
+++ src/main/java/org/hackio/hiu_api/HiuApiApplication.java
@@ -0,0 +1,13 @@
+package org.hackio.hiu_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HiuApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HiuApiApplication.class, args);
+	}
+
+}
