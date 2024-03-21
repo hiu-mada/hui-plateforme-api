d8ae43ad0c4dd372e1108fa181c30725b3875ff8
nirinaHelena <hei.nirina.7@gmail.com>
2024-03-21 20:10:09 +0300

    chore: update todo, remove some todo

--- src/main/java/com/hui/plateform/hiuplateformeapi/Config/Todo.java
+++ src/main/java/com/hui/plateform/hiuplateformeapi/Config/Todo.java
@@ -2,12 +2,8 @@ package com.hui.plateform.hiuplateformeapi.Config;
 
 public class Todo {
     /**
-     * TODO: update api.yaml
-     * TODO: add return value entity to add, update, create function
      * TODO: add OAuth2 authentication method
      * TODO: check authorisiation with all entity
      * TODO: add test
-     * TODO: update all entity attribut to be like the ressources
-     * TODO: add endpoint get all flag per team
      */
 }
